7172d37167549ea8f2e324a749f680deb1007f02
Emilie Ebert <emilieebert@Emilies-MacBook-Pro.local>
2019-07-09 10:25:05 +0300

    first two modules

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor () {
+        super();
+
+       this.state = {
+           pageTitle: "Welcome to my Portfolio",
+           isLoading: false, 
+           data:[
+               { title: "Quip", category: "eCommerce", slug: 'quip' },
+               { title: "Eventbrite", category: "Scheduling", slug: 'Eventbrite'}, 
+               { title: "Ministry Safe", category: "Enterprise", slug: 'Ministry-Safe' },
+                { title: "SwingAway", category: "eCommerce", slug: 'Swing-Away'}
+            ]
+       };
+      this.handleFilter = this.handleFilter.bind(this);
+      this.getPortfolioItems=this.getPortfolioItems.bind(this);
+    }
+    handleFilter(filter){
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
+    }
+    getPortfolioItems() {
+        axios.get('https://emilieebert.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+      }
+
+    portfolioItems() {
+
+        return this.state.data.map (item => {
+            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug}/>;
+        });
+    }
+
+   
+
+    render () {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+        this.getPortfolioItems();
+        return(
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter("eCommerce")}>
+                eCommerce
+                </button>
+                <button onClick={() => this.handleFilter("Scheduling")}>
+                Scheduling
+                </button>
+                <button onClick={() => this.handleFilter("Enterprise")}>
+                Enterprise
+                </button>
+
+                {this.portfolioItems()}
+                
+            </div>
+        )
+    }
+} 
